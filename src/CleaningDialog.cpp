8e6cbf3fc1769fed578a7c860b76706dfe338370
Atrate <Atrate@protonmail.com>
2021-07-28 00:05:51 +0200

    Update copyright notices [skip ci]
    Signed-off-by: Atrate <Atrate@protonmail.com>

--- src/CleaningDialog.cpp
+++ src/CleaningDialog.cpp
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2020 Atrate <atrate@protonmail.com>
+ * Copyright (C) 2020-2021 Atrate <atrate@protonmail.com>
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
